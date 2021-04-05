0051efc9a4fbb64c966742b3adb94be2a1123d35
Xucong Zhan <zhanxucong@gmail.com>
2021-04-05 09:08:16 -0400

    initial commit

---
+++ FakeXiecheng.API/Controllers/TestAPIController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [Route("api/shoudongapi")]
+    public class TestAPIController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
